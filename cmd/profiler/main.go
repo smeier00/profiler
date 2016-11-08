55ca566ac804ea25140f9d9c3afb7f6fbda5901d
Meier <smeier@wdhl07439ea94.corp.intuit.net>
2016-11-08 11:05:33 -0800

    fixup import paths

--- cmd/profiler/main.go
+++ cmd/profiler/main.go
@@ -7,13 +7,13 @@ import (
 	//"github.com/aws/aws-sdk-go/aws/session"
 	//"os"
 	//"os/exec"
-	"profiler/lib/metadata"
-        "profiler/lib/netstat"
-	"profiler/lib/os"
-	"profiler/lib/procs"
-	"profiler/lib/profiler"
-	"profiler/lib/software"
-	"profiler/lib/unix_files"
+	"github.com/smeier00/profiler/lib/metadata"
+	"github.com/smeier00/profiler/lib/netstat"
+	"github.com/smeier00/profiler/lib/os"
+	"github.com/smeier00/profiler/lib/procs"
+	"github.com/smeier00/profiler/lib/profiler"
+	"github.com/smeier00/profiler/lib/software"
+	"github.com/smeier00/profiler/lib/unix_files"
 	//"runtime"
 	//"time"
 )
@@ -21,38 +21,36 @@ import (
 func main() {
 	path := "/tmp/agent.log"
 
-        //#Log all parts netstat, os, metadata, software, proc, unixFiles. 
-        //netstat
-        netstat_profile := profiler.New()
-        _ = metadata.GetMetadata(netstat_profile)
-        _ = netstat.GetNetStat(netstat_profile)
-        netstat_profile.Print()
+	//#Log all parts netstat, os, metadata, software, proc, unixFiles.
+	//netstat
+	netstat_profile := profiler.New()
+	_ = metadata.GetMetadata(netstat_profile)
+	_ = netstat.GetNetStat(netstat_profile)
+	netstat_profile.Print()
 
-        //os
-        os_profile := profiler.New()
-        _ = metadata.GetMetadata(os_profile)
-        _ = os.GetOs(os_profile)
-        os_profile.Print()
+	//os
+	os_profile := profiler.New()
+	_ = metadata.GetMetadata(os_profile)
+	_ = os.GetOs(os_profile)
+	os_profile.Print()
 
-
-        //Sofware 
+	//Sofware
 	software_profile := profiler.New()
 	_ = metadata.GetMetadata(software_profile)
 	_ = software.GetSoftware(software_profile)
-        software_profile.Print()
-       
+	software_profile.Print()
 
-        //Procs
-        procs_profile := profiler.New() 
-        _ = metadata.GetMetadata(procs_profile)
+	//Procs
+	procs_profile := profiler.New()
+	_ = metadata.GetMetadata(procs_profile)
 	_ = procs.GetProcs(procs_profile)
 	procs_profile.Print()
 
-        //Procs
-        unix_files_profile := profiler.New() 
-        _ = metadata.GetMetadata(unix_files_profile)
+	//Procs
+	unix_files_profile := profiler.New()
+	_ = metadata.GetMetadata(unix_files_profile)
 	_ = unix_files.GetUnixFiles(unix_files_profile)
 	unix_files_profile.Print()
-        unix_files_profile.WriteProfile(path)
+	unix_files_profile.WriteProfile(path)
 
 }
